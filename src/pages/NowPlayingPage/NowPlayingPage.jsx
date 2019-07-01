b0833a5a1cfbcd2a984b87aa40ec9fac7162780d
Ilona <ilonaspolding@gmail.com>
2019-07-01 17:53:28 +0300

    add pagination for other pages

--- src/pages/NowPlayingPage/NowPlayingPage.jsx
+++ src/pages/NowPlayingPage/NowPlayingPage.jsx
@@ -4,26 +4,39 @@ import { MOVIE_TYPE as type } from '../../app.constants';
 import MovieList from '../../components/MovieList/MovieList';
 
 import { getMovies } from '../../api/api';
+import { FlatPagination } from '../../components/FlatPagination/FlatPagination';
 
 export default class NowPlayingPage extends React.Component {
   constructor() {
     super();
     this.state = {
       movies: [],
+      page: 1,
+      totalResults: 12,
     };
   }
 
   componentDidMount() {
-    getMovies(type.NOW_PLAYING)
-      .then((movies) => {
+    getMovies(type.NOW_PLAYING, this.state.page)
+      .then(({ movies }) => {
         this.setState({ movies });
       });
   }
 
+  changePage(page) {
+    getMovies(type.NOW_PLAYING, page)
+      .then(({ movies, totalResults }) => {
+        this.setState({ movies, totalResults });
+      });
+  }
+
   render() {
-    const { movies } = this.state;
+    const { movies, page } = this.state;
     return (
-      <MovieList movies={movies} pageTitle="Now playing movies:" />
+      <div>
+        <FlatPagination onClickPage={(e, offset) => this.changePage(offset / 10 + 1)} page={page} totalResults={this.state.totalResults} />
+        <MovieList movies={movies} pageTitle="Now playing movies:" />
+      </div>
     );
   }
 }
