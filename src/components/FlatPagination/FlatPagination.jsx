59c38b902247b9e7ac1f754d3b83f02715e20fe9
Ilona <ilonaspolding@gmail.com>
2019-07-02 16:58:23 +0300

    add propTypes and styles for flatPagination component

--- src/components/FlatPagination/FlatPagination.jsx
+++ src/components/FlatPagination/FlatPagination.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 import CssBaseline from '@material-ui/core/CssBaseline';
+import { Paper } from '@material-ui/core';
 import { createMuiTheme, MuiThemeProvider } from '@material-ui/core/styles';
 import Pagination from 'material-ui-flat-pagination';
 
 import { PAGE_COUNT } from '../../app.constants';
+
+import './flat-pagination.css';
 
 const theme = createMuiTheme();
 
@@ -27,18 +31,33 @@ export class FlatPagination extends React.Component {
     return (
       <MuiThemeProvider theme={theme}>
         <CssBaseline />
-        <Pagination
+        <Paper className="pagination-wrap" square>
+          <Pagination
         // The number of rows per page. Allow a number greater than or equal to 1.
-          limit={PAGE_COUNT}
+            limit={PAGE_COUNT}
         // The number of rows to skip. Allow a number greater than or equal to 0.
-          offset={offset}
+            offset={offset}
         // The total number of rows. Allow a number greater than or equal to 0.
-          total={totalResults}
-          onClick={this.handleClick}
-        />
+            total={totalResults}
+            onClick={this.handleClick}
+          />
+        </Paper>
       </MuiThemeProvider>
     );
   }
 }
+
+FlatPagination.defaultProps = {
+  totalResults: {
+    PAGE_COUNT: { PAGE_COUNT },
+  },
+};
+
+FlatPagination.propTypes = {
+  onClickPage: PropTypes.func.isRequired,
+  totalResults: PropTypes.shape({
+    PAGE_COUNT: PropTypes.number,
+  }),
+};
 
 export default FlatPagination;
