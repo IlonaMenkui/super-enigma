276edaa0026c082d4bfa9fedb2f3d5472b76a7da
Ilona <ilonaspolding@gmail.com>
2019-07-10 14:06:40 +0300

    add simple modal component without styles and logic

---
+++ src/components/Modal/Modal.jsx
@@ -0,0 +1,41 @@
+import React from 'react';
+
+import Typography from '@material-ui/core/Typography';
+import Modal from '@material-ui/core/Modal';
+import Button from '@material-ui/core/Button';
+
+export class SimpleModal extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      open: false,
+    };
+  }
+
+  render() {
+    const { open } = this.state;
+    return (
+      <div>
+        <Button onClick={() => this.setState({ open: true })}>Open Modal</Button>
+        <Modal
+          aria-labelledby="simple-modal-title"
+          aria-describedby="simple-modal-description"
+          open={open}
+          onClose={() => this.setState({ open: false })}
+        >
+          <div>
+            <Typography variant="h6" id="modal-title">
+            Text in a modal
+            </Typography>
+            <Typography variant="subtitle1" id="simple-modal-description">
+            Duis mollis, est non commodo luctus, nisi erat porttitor ligula.
+            </Typography>
+            <SimpleModal />
+          </div>
+        </Modal>
+      </div>
+    );
+  }
+}
+
+export default SimpleModal;
--- src/components/MovieList/MovieListItem/MovieListItem.jsx
+++ src/components/MovieList/MovieListItem/MovieListItem.jsx
@@ -7,43 +7,56 @@ import Paper from '@material-ui/core/Paper';
 import Typography from '@material-ui/core/Typography';
 import ButtonBase from '@material-ui/core/ButtonBase';
 
+import { SimpleModal } from '../../Modal/Modal';
+
 import './movie-list-item.css';
 
-export const MovieListItem = ({
-  genres, title, overview, posterPath, voteAverage, releaseDate,
-}) => (
-  <Paper className="paper">
-    <Grid container spacing={2}>
-      <Grid item>
-        <ButtonBase>
-          <img className="img" alt="poster" src={posterPath} />
-        </ButtonBase>
-      </Grid>
-      <Grid item xs={12} sm container>
-        <Grid className="overview" item xs container direction="column" spacing={2}>
-          <Grid item xs>
-            <Typography gutterBottom variant="h5">
-              {title}
-              <Typography variant="body2" color="textSecondary">
-                {releaseDate}
-              </Typography>
-            </Typography>
-            <Typography variant="body2" gutterBottom>
-              {overview}
-            </Typography>
-            <Typography variant="body2" color="textSecondary">
-              {`Genres: ${genres.join(', ')}`}
-            </Typography>
+export class MovieListItem extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+    };
+  }
+
+  render() {
+    const {
+      genres, title, overview, posterPath, voteAverage, releaseDate,
+    } = this.props;
+    return (
+      <Paper className="paper">
+        <Grid container spacing={2}>
+          <Grid item>
+            <ButtonBase>
+              <img className="img" alt="poster" src={posterPath} />
+            </ButtonBase>
+            <SimpleModal />
+          </Grid>
+          <Grid item xs={12} sm container>
+            <Grid className="overview" item xs container direction="column" spacing={2}>
+              <Grid item xs>
+                <Typography gutterBottom variant="h5">
+                  {title}
+                  <Typography variant="body2" color="textSecondary">
+                    {releaseDate}
+                  </Typography>
+                </Typography>
+                <Typography variant="body2" gutterBottom>
+                  {overview}
+                </Typography>
+                <Typography variant="body2" color="textSecondary">
+                  {`Genres: ${genres.join(', ')}`}
+                </Typography>
+              </Grid>
+            </Grid>
+            <Grid item>
+              <Chip className="chip" label={voteAverage} variant="outlined" />
+            </Grid>
           </Grid>
         </Grid>
-        <Grid item>
-          <Chip className="chip" label={voteAverage} variant="outlined" />
-        </Grid>
-      </Grid>
-    </Grid>
-  </Paper>
-);
-
+      </Paper>
+    );
+  }
+}
 
 MovieListItem.propTypes = {
   genres: PropTypes.arrayOf(PropTypes.string).isRequired,
