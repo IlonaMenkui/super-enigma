d6f8e43d9cf01953a60a3583af3715a6395fb4d3
Ilona <ilonaspolding@gmail.com>
2019-08-15 13:49:09 +0300

    delete useless story in flatPagination stories

--- src/components/FlatPagination/FlatPagination.stories.js
+++ src/components/FlatPagination/FlatPagination.stories.js
@@ -7,17 +7,9 @@ import { action } from '@storybook/addon-actions';
 import FlatPagiinon from '.';
 
 const onClichPage = action('click-on-page');
-const totalResults = 10;
+const totalResults = 20;
 const page = 1;
 
 storiesOf('FlatPagination', module)
   .add('default', () => <FlatPagiinon onClickPage={onClichPage} totalResults={totalResults} />)
-  .add('with page', () => <FlatPagiinon onClickPage={onClichPage} totalResults={totalResults} page={page} />)
-  .add('with all props',
-    () => (
-      <FlatPagiinon
-        totalResults={totalResults}
-        page={page}
-        onClickPage={onClichPage}
-      />
-    ));
+  .add('with page', () => <FlatPagiinon onClickPage={onClichPage} totalResults={totalResults} page={page} />);
