b66387f0aa185cd15db5695ad2c9d5e65b3f0093
MenkuiChan <ilonaspolding@gmail.com>
2019-10-25 12:08:47 +0300

    move some dependencies at package.json

--- src/components/Modal/modal.js
+++ src/components/Modal/modal.js
@@ -1,27 +1,27 @@
 import styled from 'styled-components';
 
 export const CloseButton = styled.button`
-    margin-left: 20px;
-    margin-right: 15px;
-    margin-left: 10px; 
-    background: none;
-    border: none;
-    cursor: pointer;
-    color: gray;
-    :hover {
-        color: black;
-    }      
+  margin-left: 20px;
+  margin-right: 15px;
+  margin-left: 10px; 
+  background: none;
+  border: none;
+  cursor: pointer;
+  color: gray;
+  :hover {
+      color: black;
+  }      
 `;
 
 export const Modal = styled.div`
-display:  ${props => props.open || 'none'};
-position: fixed;
-background: white;
-border: 1px solid black;
-border-radius: 4px;
-padding: 20px 0 20px 20px;
-width: 40%;
-top:50%;
-left:50%;
-transform: translate(-50%,-50%);
+  display:  ${props => props.open || 'none'};
+  position: fixed;
+  background: white;
+  border: 1px solid black;
+  border-radius: 4px;
+  padding: 20px 0 20px 20px;
+  width: 40%;
+  top:50%;
+  left:50%;
+  transform: translate(-50%,-50%);
 `;
