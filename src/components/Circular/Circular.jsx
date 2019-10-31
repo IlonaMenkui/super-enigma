eda836f7c8d6a539422f1c534368e70c498b9aa9
IlonaMenkui <ilonaspolding@gmail.com>
2019-10-31 17:34:24 +0300

    code refactoring

--- src/components/Circular/Circular.jsx
+++ src/components/Circular/Circular.jsx
@@ -2,14 +2,12 @@ import React from 'react';
 
 import { Svg, Wrap, Circle } from './styles';
 
-function Circular() {
-  return (
-    <Wrap>
-      <Svg viewBox="0 0 100 100">
-        <Circle />
-      </Svg>
-    </Wrap>
-  );
-}
+const Circular = () => (
+  <Wrap>
+    <Svg viewBox="0 0 100 100">
+      <Circle />
+    </Svg>
+  </Wrap>
+);
 
 export default Circular;
