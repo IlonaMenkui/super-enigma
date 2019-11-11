5228c0645263c75f09f114145223ef328e8325c4
IlonaMenkui <ilonaspolding@gmail.com>
2019-11-11 13:16:34 +0300

    delete unnessesary styles for cross img

--- src/components/Modal/style.js
+++ src/components/Modal/style.js
@@ -4,8 +4,6 @@ import styled from 'styled-components';
 export const CloseCross = styled.img`
   margin-left: 20px;
   margin-right: 15px;
-  background: none;
-  border: none;
   cursor: pointer;
   width: 13px;
 `;
