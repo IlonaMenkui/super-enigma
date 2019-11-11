3a3d3fcf26ac242a5d05ff06d91a24886368c658
IlonaMenkui <ilonaspolding@gmail.com>
2019-11-11 17:35:32 +0300

    add some conditions for pagination

--- src/components/Pagination/Pagination.jsx
+++ src/components/Pagination/Pagination.jsx
@@ -6,11 +6,23 @@ import { PageNumber, PaginationWrap } from './style';
 function Pagination({ onClickPage, totalPages: pagesCount, page }) {
   const currentPage = page;
   const lastPage = pagesCount;
+  const secondPage = currentPage + 1;
+  const thirdPage = currentPage + 2;
+  const firstPages = [];
+  if (secondPage === lastPage) {
+    firstPages.push(currentPage, secondPage); // когда вторая страница - последняя
+  } else if (currentPage !== 1 && currentPage === lastPage) {
+    firstPages.push(1, currentPage); // когда всего две страницы и актуальная - вторая
+  } else if (currentPage !== lastPage && thirdPage !== lastPage && pagesCount > 3) {
+    firstPages.push(currentPage, secondPage, thirdPage);
+  } else if (pagesCount === 0) {
+    firstPages.push(0);
+  }
 
   const handleClick = pageNumber => {
-    if (pageNumber < 1) {
+    if (pageNumber < 1 && pageNumber !== 0) {
       onClickPage(1);
-    } else if (pageNumber <= pagesCount) {
+    } else if (pageNumber <= pagesCount && pageNumber !== 0) {
       onClickPage(pageNumber);
     }
   };
@@ -19,7 +31,7 @@ function Pagination({ onClickPage, totalPages: pagesCount, page }) {
     <PaginationWrap>
       <PageNumber onClick={() => handleClick(1)}>{'<<'}</PageNumber>
       <PageNumber onClick={() => handleClick(currentPage - 1)}>{'<'}</PageNumber>
-      {[currentPage, currentPage + 1, currentPage + 2]
+      {firstPages
         .map(pageNumber => (
           <PageNumber
             onClick={() => handleClick(pageNumber)}
