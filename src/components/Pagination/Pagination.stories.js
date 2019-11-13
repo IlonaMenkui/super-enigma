e6cc30bcb9bee5268ecaa69d28519858c344cfde
IlonaMenkui <ilonaspolding@gmail.com>
2019-11-13 16:25:34 +0300

    add storybook test for pagination

--- src/components/Pagination/Pagination.stories.js
+++ src/components/Pagination/Pagination.stories.js
@@ -5,16 +5,12 @@ import { action } from '@storybook/addon-actions';
 
 import Pagiinon from '.';
 
-const onClickPage = action('click-on-page');
-const totalResults = 100;
+const changePage = action('click-on-page');
+const totalPages = 100;
 const page = 3;
 
 storiesOf('Pagination', module)
-  .add('default', () => <Pagiinon onClickPage={onClickPage} page={page} totalResults={totalResults} />)
-  .add('only with total results', () => <Pagiinon onClickPage={onClickPage} totalResults={totalResults} />)
-  .add('with selected page', () => <Pagiinon onClickPage={onClickPage} page={page} totalResults={totalResults} />)
-  .add('without any data', () => <Pagiinon onClickPage={onClickPage} />);
-
-  onClickPage={pageNew => this.changePage(pageNew)}
-            page={page}
-            totalPages={totalPages}
+  .add('default', () => <Pagiinon onClickPage={changePage} page={page} totalPages={totalPages} />)
+  .add('only with total results', () => <Pagiinon onClickPage={changePage} totalPages={totalPages} />)
+  .add('with selected page', () => <Pagiinon onClickPage={changePage} page={page} totalPages={totalPages} />)
+  .add('without any data', () => <Pagiinon onClickPage={changePage} />);
