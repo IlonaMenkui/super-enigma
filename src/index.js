5780f4b9bbf7d92ddc5e0cf0e44ff47a9201d1bb
IlonaMenkui <ilonaspolding@gmail.com>
2019-11-27 16:20:53 +0300

    add redux-saga package

--- src/index.js
+++ src/index.js
@@ -4,16 +4,18 @@ import ReactDOM from 'react-dom';
 import { createStore, applyMiddleware } from 'redux';
 import { Provider } from 'react-redux';
 import { createLogger } from 'redux-logger';
+import createSagaMiddleware from 'redux-saga'
 
 import movies from './reducers';
 import App from './app/App';
 
 import './index.css';
 
+const sagaMiddleware = createSagaMiddleware();
 const logger = createLogger();
 const store = createStore(
   movies,
-  applyMiddleware(logger),
+  applyMiddleware(logger, sagaMiddleware),
 );
 
 ReactDOM.render(
