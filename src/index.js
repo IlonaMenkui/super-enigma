c82df2df424e1f77f1c806820e44331c69d44e7e
IlonaMenkui <ilonaspolding@gmail.com>
2019-11-28 11:26:44 +0300

    add sagas file into the project

--- src/index.js
+++ src/index.js
@@ -1,22 +1,26 @@
+import 'babel-polyfill';
 import React from 'react';
 import ReactDOM from 'react-dom';
 
 import { createStore, applyMiddleware } from 'redux';
 import { Provider } from 'react-redux';
 import { createLogger } from 'redux-logger';
-import createSagaMiddleware from 'redux-saga'
+import createSagaMiddleware from 'redux-saga';
 
 import movies from './reducers';
 import App from './app/App';
+import helloSaga from './sagas/sagas';
 
 import './index.css';
 
 const sagaMiddleware = createSagaMiddleware();
+
 const logger = createLogger();
 const store = createStore(
   movies,
   applyMiddleware(logger, sagaMiddleware),
 );
+sagaMiddleware.run(helloSaga);
 
 ReactDOM.render(
   <Provider store={store}>
